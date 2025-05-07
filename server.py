0e7f54fea4f7a398a93f007fee7c710e5ec46669
prathambande <81869581+prathambande@users.noreply.github.com>
2025-05-07 15:22:29 +0530

    Update server.py

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello OK World!"
+    return "Hello Hi hey World!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
